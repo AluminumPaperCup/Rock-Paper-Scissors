251570a80c75e7a03438c78ccb3cada4bb06b4a1
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-05-23 14:58:00 -0400

    Computer can now pick a random number to choose

---
+++ script.js
@@ -0,0 +1,15 @@
+ //// Your JavaScript goes here!
+
+  // remember basic function syntax!!
+
+  // function nameOfFunction(a, b)  {
+  //  return (a , b);
+  // }
+
+// Create a new function named getComputerChoice
+// break down problem into components
+// see if we can get the computer to return just rock, it doesn't have to be random
+
+let computerChoice = Math.random();
+console.log (computerChoice);
+
