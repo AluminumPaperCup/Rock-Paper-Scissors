6a4a1858c93803662f4dd45e07fd3d4bfbeef77c
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-05-24 15:37:21 -0400

    FIX computer calculation of random variable This is was because the function couldn't be called

--- script.js
+++ script.js
@@ -10,28 +10,38 @@
 // break down problem into components
 // see if we can get the computer to return just rock, it doesn't have to be random
 
-let computerChoice = Math.random();
-console.log (computerChoice);
+// These are the Global Scope variables
+let computerChoice = getComputerChoice();
+
+let humanChoice = getHumanChoice();
 
 let humanScore = 0;
 console.log(humanScore);
 
 let computerScore = 0;
 console.log(computerScore);
+// These are the Global Scope variables
 
+
+// Function that allows the Human Choice to be selected and is case insensitive
 function getHumanChoice() {
- console.log(prompt("Type in Rock, Paper, or Scissors!"));
+ console.log(prompt("Type in Rock, Paper, or Scissors!").toLowerCase());
 }
 
+// Function that allows the Computer to make a choice and categorize it as Rock, Paper, or Scissors
 function getComputerChoice() {
-  if (computerChoice <= 0.33 ) {
+  if (Math.random() <= 0.33 ) {
       alert("Rock!");
   }
-  else if (computerChoice <= 0.67 ) {
+  else if (Math.random() <= 0.66 ) {
     alert("Paper!");
 }
-  else if (computerChoice > 0.67) {
+  else if (Math.random() > 0.66) {
     alert("Scissors!");
 }
 
+}
+
+function playRound(humanChoice, computerChoice) {
+  
 }
