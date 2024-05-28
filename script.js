b15a9651460e33d782c6e5c769de6130905deee1
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-05-28 12:54:38 -0400

    Reconstructed global variables and changed computer choice function

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
  //// Your JavaScript goes here!
 
-// These are the Global Scope variables
-const humanSelection = getHumanChoice();
-
-const computerSelection = getComputerChoice();
-
 let humanScore = 0;
-console.log(humanScore);
-
 let computerScore = 0;
-console.log(computerScore);
-// These are the Global Scope variables
-
-
-// Function that allows the Human Choice to be selected and is case insensitive
-function getHumanChoice() {
- console.log(prompt("Type in Rock, Paper, or Scissors!").toLowerCase());
-}
 
 // Function that allows the Computer to make a choice and categorize it as Rock, Paper, or Scissors
 function getComputerChoice() {
-  if (Math.random() <= 0.33 ) {
-      alert("Rock!");
-  }
-  else if (Math.random() >= 0.66 ) {
-    alert("Paper!");
-}
-  else if (Math.random()) {
-    alert("Scissors!");
-}
-
-}
-
-function playRound(humanChoice, computerChoice) {
-    
+    if (Math.random() <= 0.33 ) {
+      return ("rock");
+    }
+    else if (Math.random() >= 0.66 ) {
+      return ("paper");
+    }
+    else if (Math.random()) {
+      return ("scissors");
+    }
 }
