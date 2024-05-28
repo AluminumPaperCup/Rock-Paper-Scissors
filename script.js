5606a8c036bfe61215c82e77dafc1da8085d9da5
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-05-28 13:12:46 -0400

    ADD human and computer score elements

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ let computerScore = 0;
 
 // Function that allows the Computer to make a choice and categorize it as Rock, Paper, or Scissors
 function getComputerChoice() {
-    if (Math.random() <= 0.33 ) {
-      return ("rock");
+  if (Math.random() <= 0.33 ) {
+    return ("rock");
     }
-    else if (Math.random() >= 0.66 ) {
-      return ("paper");
+  else if (Math.random() >= 0.66 ) {
+    return ("paper");
     }
-    else if (Math.random()) {
-      return ("scissors");
+  else if (Math.random()) {
+    return ("scissors");
     }
 }
 
@@ -23,20 +23,19 @@ function playRound() {
     return (`Ya'll both suck! You both picked ${humanSelection}.`);
   } 
   else if (humanSelection === "rock" && computerSelection === "scissors") {
-
+    humanScore += 1;
     return (`You win! You picked ${humanSelection}.`)  
   }
   else if (humanSelection === "paper" && computerSelection === "rock") {
-
+    humanScore += 1;
     return (`You win! You picked ${humanSelection}.`);
   }
   else if (humanSelection === "scissors" && computerSelection === "paper") {
-
+    humanScore += 1;
     return (`You win! You picked ${humanSelection}.`);
   }
   else {
-
+    computerScore +=1;
     return (`You lose! Computer chose ${computerSelection} and that beats ${humanSelection}`);
-  
-}
+  }
 }
