9f43bf1230e8a96b893562b9f459915be02c1db7
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-08-01 12:51:43 -0400

    REMOVE human selection that uses alert from playRound function in order to facilitate GUI buttons as selection

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function getComputerChoice() {
 //Function that plays 1 round of game (game logic)
 function playRound(humanSelection, computerSelection) {
   computerSelection = getComputerChoice();
-  humanSelection = prompt("Choose 'Rock, Paper, or Scissors' ").toLowerCase();
  
   if (humanSelection === computerSelection) {
     return (`Ya'll both suck! You both picked ${humanSelection}.`);
