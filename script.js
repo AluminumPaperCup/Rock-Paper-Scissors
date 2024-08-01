8461502800179c3eee2d01882c7fee7768a3d34a
Brandon <104651603+AluminumPaperCup@users.noreply.github.com>
2024-08-01 14:55:48 -0400

    FIX  add result back into game function as this was needed in update functions

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice() {
 function game(buttonId) {
   let computerSelection = getComputerChoice();
   let humanSelection = buttonId;
-  playRound(humanSelection, computerSelection);
+  let result = playRound(humanSelection, computerSelection);
   };
 
   //Event Listeners for each button selection of human selection
